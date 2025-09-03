29233254983b985bf069b5e57f92ebfc7058b3ac
rs nazia <rsscalers042@gmail.com>
2025-09-03 15:27:37 +0600

    Coins decreasing function added

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ document.getElementById("Heart").addEventListener("click", function(){
 })
 
 
+function doCall(id){
+    const coin =parseInt( document.getElementById(id).innerText)
+        if(coin >20){
+            alert("You click on the call button");
+          const  Remaincoin = coin - 20
+           document.getElementById(id).innerText = Remaincoin;
+        }
+        else{
+            alert("You dont have enough coin!")
+        }
+  }
 
-function getCopy(){
-    
-}
+ document.getElementById("card-btn")
+    .addEventListener("click", function(){
+        doCall("coins");
+    })
